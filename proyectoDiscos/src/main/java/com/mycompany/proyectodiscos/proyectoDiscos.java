deae3c17d3c9a0b531c4c1a6dca4e696c92adfdd
JO970 <oscar_bl789@hotmail.com>
2021-08-07 13:28:59 -0500

    Se corrige el "error" Unused assignement es que no declares nada con valores que no vayas a usar.

--- proyectoDiscos/src/main/java/com/mycompany/proyectodiscos/proyectoDiscos.java
+++ proyectoDiscos/src/main/java/com/mycompany/proyectodiscos/proyectoDiscos.java
@@ -5,7 +5,7 @@ class proyectoDiscos
 {
     public static void main( String[] args )
     {
-        int a = 15, b = 10, c = 0;
+        int a = 15, b = 10, c ;
         c = a + b;
         System.out.println(" La suma de a y b es: " + c);
 
