da51d1d7c5960c653f11f099be72c0ec0bd89c7b
JO970 <oscar_bl789@hotmail.com>
2021-08-07 13:19:39 -0500

    Cambie el codigo de Hola mundo, por uno de suma de dos números

--- proyectoDiscos/src/main/java/com/mycompany/proyectodiscos/proyectoDiscos.java
+++ proyectoDiscos/src/main/java/com/mycompany/proyectodiscos/proyectoDiscos.java
@@ -1,22 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.proyectodiscos;
 
-/**
- *
- * @author hello
- */
-public class proyectoDiscos {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola mundo");
+class proyectoDiscos
+{
+    public static void main( String[] args )
+    {
+        int a = 15, b = 10, c = 0;
+        c = a + b;
+        System.out.println(" La suma de a y b es: " + c);
+
     }
-    
 }
